7dd43d519682860c99f76d97a23bb11d2bc62030
Arjun  Dass <ad.sam007@gmail.com>
2017-03-05 18:21:19 -0500

    Add commit hw6

--- SSW-555 - Agile methods of Software Development/Homework/hw6/hw6.py
+++ SSW-555 - Agile methods of Software Development/Homework/hw6/hw6.py
@@ -6,16 +6,21 @@ date2 = datetime.date(2016, 10, 3)
 def number_of_days_between_two_dates(date_1,date_2):
 	delta = date2 - date1
 	print("No of days between " + str(date_1) + " and " + str(date_2) + " are " + str(delta.days))
+	return int(delta.days)
 
 def number_of_months_between_two_dates(date_1,date_2):
 	delta = date2 - date1
-	print("No of months between " + str(date_1) + " and " + str(date_2) + " are " + str(int(delta.days/30.4)))
+	months = int(delta.days/30.4)
+	print("No of months between " + str(date_1) + " and " + str(date_2) + " are " + str(months))
+	return months
 
 def number_of_years_between_two_dates(date_1,date_2):
 	delta = date2 - date1
-	print("No of years between " + str(date_1) + " and " + str(date_2) + " are " + str(int(delta.days/365.25)))
+	years = int(delta.days/365.25)
+	print ("No of years between " + str(date_1) + " and " + str(date_2) + " are " + str(years))
+	return years
 
 if __name__ == '__main__':
-	number_of_days_between_two_dates(date1,date2)
-	number_of_months_between_two_dates(date1,date2)
-	number_of_years_between_two_dates(date1,date2)
+	print(number_of_days_between_two_dates(date1,date2))
+	print(number_of_months_between_two_dates(date1,date2))
+	print(number_of_years_between_two_dates(date1,date2))
